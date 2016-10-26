925f6c89d64cf0ea2db6a390ffca7c607c1a6bd6
Chris Marlow <christopher.marlow@shopify.com>
2016-10-26 02:26:29 -0400

    cleanup

---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavHelper from '../components/nav-helper'
+
+export default React.createClass({
+  render () {
+    return (
+      <NavHelper className='container'>
+        <header role='banner'>
+          <h1>Home</h1>
+        </header>
+      </NavHelper>
+    )
+  }
+})
--- src/router.js
+++ src/router.js
@@ -1,24 +1,14 @@
-import app from 'ampersand-app'
+import app    from 'ampersand-app'
 import Router from 'ampersand-router'
-import React from 'react'
-import qs from 'qs'
-import xhr from 'xhr'
-import PublicPage from './pages/public'
-import ReposPage from './pages/repos'
-import RepoDetail from './pages/repo-detail'
-import Layout from './layout'
-import MessagePage from './pages/message'
+import React  from 'react'
+import qs     from 'qs'
+import xhr    from 'xhr'
 import config from './config'
 
-function requiresAuth (handlerName) {
-  return function () {
-    if (app.me.token) {
-      this[handlerName].apply(this, arguments)
-    } else {
-      this.redirectTo('/')
-    }
-  }
-}
+// React pages
+import Layout      from './layout'
+import MainPage  from './pages/main'
+import MessagePage from './pages/message'
 
 export default Router.extend({
   renderPage (page, opts = {layout: true}) {
@@ -35,65 +25,13 @@ export default Router.extend({
 
   routes: {
     '': 'public',
-    'repos': requiresAuth('repos'),
-    'login': 'login',
-    'logout': 'logout',
-    'repo/:owner/:name': requiresAuth('repoDetail'),
-    'auth/callback?:query': 'authCallback',
     '*fourOhfour': 'fourOhfour'
   },
 
   public () {
-    this.renderPage(<PublicPage/>, {layout: false})
-  },
-
-  repos () {
-    this.renderPage(<ReposPage repos={app.me.repos}/>)
-  },
-
-  repoDetail (owner, name) {
-    const model = app.me.repos.getByFullName(owner + '/' + name)
-    this.renderPage(<RepoDetail repo={model} labels={model.labels}/>)
-  },
-
-  login () {
-    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
-      client_id: config.clientId,
-      redirect_uri: window.location.origin + '/auth/callback',
-      scope: 'user,repo'
-    })
-  },
-
-  authCallback (query) {
-    query = qs.parse(query)
-    console.log(query)
-
-    xhr({
-      url: config.authUrl + '/' + query.code,
-      json: true
-    }, (err, req, body) => {
-      console.log(body)
-      app.me.token = body.token
-      this.redirectTo('/repos')
-    })
-
-    this.renderPage(<MessagePage title='Fetching your data'/>)
-  },
-
-  logout () {
-    window.localStorage.clear()
-    window.location = '/'
+    this.renderPage(<MainPage/>, {layout: false})
   },
 
   fourOhfour () {
-    this.renderPage(<MessagePage title='Not Found' body='sorry nothing here'/>)
-  }
+      this.renderPage(<MessagePage title='Not Found' body='sorry nothing here'/>) }
 })
-
-
-
-
-
-
-
-
